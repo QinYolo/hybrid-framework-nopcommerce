16fa1edccfb5a81464f5e354f578406c9db1a3a7
Qin Yolo <canthivanha@gmail.com>
2023-04-30 00:49:13 +0700

    Update extent report V3 - V4 - V5

--- actions/commons/GlobalConstant.java
+++ actions/commons/GlobalConstant.java
@@ -8,6 +8,7 @@ public class GlobalConstant {
 	public static final String STAGING_PAGE_URL = "https://staging.nopcommerce.com/";
 	public static final String PRODUCTION_PAGE_URL = "https://production.nopcommerce.com/";
 	public static final String PROJECT_PATH = System.getProperty("user.dir");
+	public static final String JAVA_VERSION = System.getProperty("java.version");
 	public static final String OS_NAME = System.getProperty("os.name");
 	public static final String EXTENT_PATH = PROJECT_PATH + File.separator + "extentV2" +  File.separator;
 	public static final String UPLOAD_FILE = PROJECT_PATH + File.separator + "uploadFiles" +  File.separator;
