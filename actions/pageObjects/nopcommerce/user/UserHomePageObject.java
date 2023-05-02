2f171097f5247775a4676a90ed13347f8f15c644
Qin Yolo <canthivanha@gmail.com>
2023-05-03 01:43:02 +0700

    Adding Allure report

--- actions/pageObjects/nopcommerce/user/UserHomePageObject.java
+++ actions/pageObjects/nopcommerce/user/UserHomePageObject.java
@@ -3,6 +3,7 @@ package pageObjects.nopcommerce.user;
 import org.openqa.selenium.WebDriver;
 
 import commons.BasePage;
+import io.qameta.allure.Step;
 import pageUIs.nopcommerce.user.UserHomePageUI;
 
 public class UserHomePageObject extends BasePage{
@@ -13,18 +14,21 @@ public class UserHomePageObject extends BasePage{
 		this.driver = driver;
 	}
 
+	@Step("Navigate to register page")
 	public UserRegisterPageObject clickToRegisterLink() {
 		waitForElementClickable(driver, UserHomePageUI.REGISTER_LINK);
 		clickToElement(driver, UserHomePageUI.REGISTER_LINK);
 		return PageGeneratorManager.getUserRegisterPage(driver);
 	}
 
+	@Step("Navigate to login page")
 	public UserLoginPageObject clickToLoginLink() {
 		waitForElementClickable(driver, UserHomePageUI.LOGIN_LINK);
 		clickToElement(driver, UserHomePageUI.LOGIN_LINK);
 		return PageGeneratorManager.getUserLoginPage(driver);
 	}
 
+	@Step("Verify 'My Account' link to displayed")
 	public boolean isMyAccountLinkDisplayed() {
 		waitForElementVisible(driver, UserHomePageUI.MY_ACCOUNT_LINK);
 		return isElementDisplayed(driver, UserHomePageUI.MY_ACCOUNT_LINK);
